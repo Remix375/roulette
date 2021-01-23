abca1cd92ada86d4d05d209bd87bc8097f21974d
Remi Guillou <bigbossremi@gmail.com>
2021-01-23 20:15:03 +0100

    balance system

--- ressource/js/roll.js
+++ ressource/js/roll.js
@@ -1,17 +1,25 @@
 let rollButton = document.getElementById('roll');
-console.log(rollButton)
+
 let roulette = document.getElementById('roulette');
 
+let balanceElement = document.getElementById('balance')
+
+
 let angle = 0;
+let balance = 1000;
 
 const roll = () => {
-    const degrees = Math.floor(Math.random() * 1900) + 400
-    for (let i = 0; i< degrees; i++) {
-        roulette.style.transform = `rotate(${angle}deg)`
-        angle++ 
-    }
-    console.log(angle)
+    balance -= 10
+    balanceElement.innerHTML = `Balance: ${balance}`
+    const degrees = Math.floor(Math.random() * 5000) + 400
+    angle = degrees
+    roulette.style.transform = `rotate(${angle}deg)`
+
+    
+
 }
+
+
 
 
 
