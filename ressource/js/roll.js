e511eaf9097acc2e694df44e785ff3b996157d8c
Remi Guillou <bigbossremi@gmail.com>
2021-01-27 15:54:57 +0100

    arrow test

--- ressource/js/roll.js
+++ ressource/js/roll.js
@@ -1,26 +1,42 @@
 let rollButton = document.getElementById('roll');
 
-let roulette = document.getElementById('roulette');
+let roulette = document.getElementById('roue');
 
 let balanceElement = document.getElementById('balance')
 
 
 let angle = 0;
 let balance = 1000;
+let chosenColor = ''
+
 
 const roll = () => {
-    balance -= 10
-    balanceElement.innerHTML = `Balance: ${balance}`
-    const degrees = Math.floor(Math.random() * 5000) + 400
+    let deg = randomdegree()
+
+    const degrees = Math.floor(Math.random() * 720) + 400
+
     angle = degrees
     roulette.style.transform = `rotate(${angle}deg)`
-
     
+    balance -= 10
+    balanceElement.innerHTML = `Balance: ${balance}`
+}
+
+
+const randomdegree = () => {
+    randomdeg = Math.random() * 360
+    return randomdeg
+}
+
+const getColor = (degree) => {
 
 }
 
 
-
+//a changer
+const setChosenColor = (color) => {
+    chosenColor = color
+}
 
 
 rollButton.onclick = roll;
